1aa5bdc6cce56b23a70d147211b98b9b1b0191ad
logisoltech <operations@logisol.tech>
2026-05-04 17:06:58 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -31,7 +31,7 @@ export default function Home() {
       />
       <Video />
       <FutureSwimSection />
-      <FlyerParkDesignSection />
+      {/* <FlyerParkDesignSection /> */}
       <Footer />
     </div>
   );
